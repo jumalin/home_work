32c5f4b0c32ddffd465837073ca0d913fadf645b
djumalin <jumalin.daniyar@gmail.com>
2023-03-17 14:27:30 +0100

    HW6 WIP

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,27 @@ type (
 
 type Stage func(in In) (out Out)
 
+func ConcurrentStage(done In, in In, stage Stage) Out {
+	addedStream := make(Bi)
+	go func() {
+		defer close(addedStream)
+		for i := range stage(in) {
+			select {
+			case <-done:
+				return
+			case addedStream <- i:
+			}
+		}
+	}()
+	return addedStream
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
+
 	// Place your code here.
-	return nil
+	proxyChannel := in
+	for _, stage := range stages {
+		proxyChannel = ConcurrentStage(done, proxyChannel, stage)
+	}
+	return proxyChannel
 }
